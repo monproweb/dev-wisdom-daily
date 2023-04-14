3e382cef5fdf0ff9d0bd904be9bd860201d0d7c7
Thomas Erhel <thomas.erhel@gmail.com>
2023-04-14 02:57:03 +0200

    Fix: trigger_tweet and main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from twitter_utils import setup_tweepy_api, tweet_quote_and_image
-from gpt_utils import get_previous_quotes
+from gpt_utils import TWITTER_ACCOUNT, get_previous_quotes, generate_quote, generate_image
 
 
 def trigger_tweet(event, context):
@@ -7,8 +7,10 @@ def trigger_tweet(event, context):
     Trigger function to tweet a developer quote and image.
     """
     API = setup_tweepy_api()
-    previous_quotes = get_previous_quotes(API)
-    tweet_quote_and_image(API, previous_quotes)
+    previous_quotes = get_previous_quotes(API, TWITTER_ACCOUNT)
+    quote, image_description = generate_quote(API, previous_quotes)
+    image_url = generate_image(image_description)
+    tweet_quote_and_image(API, quote, image_url)
 
 
 def main():
@@ -16,8 +18,10 @@ def main():
     The main function that triggers the tweet_quote_and_image() function.
     """
     API = setup_tweepy_api()
-    previous_quotes = get_previous_quotes(API)
-    tweet_quote_and_image(API, previous_quotes)
+    previous_quotes = get_previous_quotes(API, TWITTER_ACCOUNT)
+    quote, image_description = generate_quote(API, previous_quotes)
+    image_url = generate_image(image_description)
+    tweet_quote_and_image(API, quote, image_url)
 
 
 if __name__ == "__main__":
