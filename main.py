fa4be8efe2f4f24d1f9cfe71f9f5120d35c93c7b
Thomas Erhel <thomas.erhel@gmail.com>
2023-04-15 08:42:32 +0200

    Add max_tokens

--- main.py
+++ main.py
@@ -15,7 +15,12 @@ TWITTER_ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 # Check API keys and access tokens
-if not TWITTER_API_KEY or not TWITTER_API_SECRET or not TWITTER_ACCESS_TOKEN or not TWITTER_ACCESS_TOKEN_SECRET:
+if (
+    not TWITTER_API_KEY
+    or not TWITTER_API_SECRET
+    or not TWITTER_ACCESS_TOKEN
+    or not TWITTER_ACCESS_TOKEN_SECRET
+):
     raise ValueError("Please provide Twitter API keys and access tokens.")
 if not OPENAI_API_KEY:
     raise ValueError("Please provide OpenAI API key.")
@@ -86,7 +91,7 @@ def is_quote_similar(quote, previous_quotes_text, similarity_threshold=90):
     Returns:
         bool: True if the quote is similar, False otherwise.
     """
-    for prev_quote in previous_quotes_text.split('\n'):
+    for prev_quote in previous_quotes_text.split("\n"):
         similarity = fuzz.token_set_ratio(quote, prev_quote)
         if similarity >= similarity_threshold:
             return True
@@ -125,7 +130,7 @@ def generate_image(prompt):
         response_format="url",
     )
 
-    image_url = response.data[0]['url']
+    image_url = response.data[0]["url"]
     return image_url
 
 
@@ -165,20 +170,26 @@ def generate_quote(API, previous_quotes_text):
     while True:
         chat_messages = [
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "assistant",
-                "content": f"Here are some previous quotes:\n{previous_quotes_text}"},
-            {"role": "user", "content": "Provide an existing quote from a well-known software developer, programmer, software engineer or tech figure, along with their name. Include a maximum of 1-2 related hashtags for Twitter. Keep your copy short and sweet. Add in emoji or a touch of sass or silliness — and let the engagement be your guide. Your Tweet can contain up to 280 characters maximum, formatted starting with the quote followed by the name and be conversational at the end."},
+            {
+                "role": "assistant",
+                "content": f"Here are some previous quotes:\n{previous_quotes_text}",
+            },
+            {
+                "role": "user",
+                "content": "Provide an existing quote from a well-known software developer, programmer, software engineer or tech figure, along with their name. Include a maximum of 1-2 related hashtags for Twitter. Keep your copy short and sweet. Add in emoji or a touch of sass or silliness — and let the engagement be your guide. Your Tweet can contain up to 280 characters maximum, formatted starting with the quote followed by the name and be conversational at the end.",
+            },
         ]
 
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=chat_messages,
             n=1,
+            max_tokens=70,
             stop=None,
             temperature=0.7,
         )
 
-        quote = response.choices[0].message['content'].strip()
+        quote = response.choices[0].message["content"].strip()
         quote_text = extract_quote_from_tweet(quote)
 
         # Check if the generated quote is similar to previous quotes
@@ -203,7 +214,10 @@ def generate_detailed_description(quote_text, examples):
 
     chat_messages = [
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": f"Describe the quote '{quote_text}' visually with very detailed elements up to 1000 characters for generating an image, making sure there is absolutely NO text on the image. The image should only contain visuals that represent the idea behind the quote. Please provide the description in a single block of text without line breaks. Also, consider the following examples for inspiration: {example_text}"},
+        {
+            "role": "user",
+            "content": f"Describe the quote '{quote_text}' visually with very detailed elements up to 1000 characters for generating an image, making sure there is absolutely NO text on the image. The image should only contain visuals that represent the idea behind the quote. Please provide the description in a single block of text without line breaks. Also, consider the following examples for inspiration: {example_text}",
+        },
     ]
 
     response = openai.ChatCompletion.create(
@@ -214,7 +228,7 @@ def generate_detailed_description(quote_text, examples):
         temperature=0.7,
     )
 
-    detailed_description = response.choices[0].message['content'].strip()
+    detailed_description = response.choices[0].message["content"].strip()
     return detailed_description
 
 
@@ -251,7 +265,7 @@ def tweet_quote_and_image(API):
     """
     try:
         previous_quotes = get_previous_quotes(API)
-        previous_quotes_text = '\n'.join(previous_quotes)
+        previous_quotes_text = "\n".join(previous_quotes)
 
         quote, quote_text = generate_quote(API, previous_quotes_text)
         print(f"Generated quote: {quote}")
@@ -301,8 +315,7 @@ def tweet_quote_and_image(API):
             "A 3D render of a rainbow colored hot air balloon flying above a reflective lake",
         ]
 
-        detailed_description = generate_detailed_description(
-            quote_text, examples)
+        detailed_description = generate_detailed_description(quote_text, examples)
         print(f"Generated detailed description: {detailed_description}")
 
         image_url = generate_image(detailed_description)
