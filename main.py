4269bfe9ee36dcd7966a6d46b40f830b58cfb143
Thomas Erhel <thomas.erhel@gmail.com>
2023-06-28 15:09:08 +0200

    Update twitter count

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def get_previous_quotes(API):
     all_tweets = tweepy.Cursor(
         API.user_timeline,
         screen_name=TWITTER_ACCOUNT,
-        count=60,
+        count=200,
         tweet_mode="extended",
     ).items()
 
