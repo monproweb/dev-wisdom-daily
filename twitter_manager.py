06e1bbb67c3e20ab3a7144538818d4a112058ae0
Thomas Erhel <thomas.erhel@gmail.com>
2023-07-15 16:26:09 +0200

    Enhanced Threads error handling

--- twitter_manager.py
+++ twitter_manager.py
@@ -12,15 +12,6 @@ import json
 
 
 def setup_tweepy_client(config):
-    """
-    Set up the Tweepy Client object using the provided API keys and access tokens.
-
-    Args:
-        config (dict): A dictionary containing the API keys and access tokens.
-
-    Returns:
-        tweepy.Client: An instance of the Tweepy Client object.
-    """
     client = tweepy.Client(
         consumer_key=config["TWITTER_API_KEY"],
         consumer_secret=config["TWITTER_API_SECRET"],
@@ -31,15 +22,6 @@ def setup_tweepy_client(config):
 
 
 def setup_tweepy_api(config):
-    """
-    Set up the Tweepy API object using the provided API keys and access tokens.
-
-    Args:
-        config (dict): A dictionary containing the API keys and access tokens.
-
-    Returns:
-        tweepy.API: An instance of the Tweepy API object.
-    """
     auth = tweepy.OAuthHandler(config["TWITTER_API_KEY"], config["TWITTER_API_SECRET"])
     auth.set_access_token(
         config["TWITTER_ACCESS_TOKEN"], config["TWITTER_ACCESS_TOKEN_SECRET"]
@@ -48,16 +30,6 @@ def setup_tweepy_api(config):
 
 
 def upload_media(url, API):
-    """
-    Upload an image to Twitter from a given URL.
-
-    Args:
-        url (str): The URL of the image to be uploaded.
-        API (tweepy.API): An instance of the Tweepy API object.
-
-    Returns:
-        str: The media ID string of the uploaded image.
-    """
     response = requests.get(url)
     image_data = BytesIO(response.content)
 
@@ -66,15 +38,6 @@ def upload_media(url, API):
 
 
 def handle_error(e):
-    """
-    Handle various errors that may occur during interaction with the Twitter and OpenAI APIs.
-
-    This function will print an error message based on the type of error encountered, providing
-    additional information for API errors and suggesting a solution for unauthorized access.
-
-    Args:
-        e (Exception): The exception raised during interaction with the APIs.
-    """
     if isinstance(e, tweepy.errors.Unauthorized):
         print(f"An error occurred while interacting with the Twitter API: {e}")
         if e.api_codes and 89 in e.api_codes:
@@ -90,12 +53,6 @@ def handle_error(e):
 
 
 def tweet_quote_and_image(client, API, config):
-    """
-    Generate a quote, a detailed description, an image, and tweet them.
-
-    Args:
-        client (tweepy.Client): The Tweepy Client object.
-    """
     content_generator = ContentGenerator(client)
 
     settings = None
@@ -155,3 +112,10 @@ def tweet_quote_and_image(client, API, config):
 
     except Exception as e:
         print("An error occurred while interacting with Threads: ", e)
+
+    except Exception as e:
+        try:
+            client.create_tweet(text=quote)
+            print(f"Tweeted: {quote}")
+        except Exception as e:
+            print("An error occurred while trying to tweet: ", e)
