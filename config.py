2ade14f623a69ec68563d09cc5da300dc9a0140f
Thomas Erhel <thomas.erhel@gmail.com>
2023-07-19 07:42:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,43 @@
 import os
+import base64
+import requests
+from dotenv import load_dotenv
+
+
+def get_bearer_token(api_key, api_secret_key):
+    """
+    Retrieve the Bearer token from Twitter's API.
+
+    Args:
+        api_key (str): The API key.
+        api_secret_key (str): The API secret key.
+
+    Returns:
+        str: The Bearer token.
+
+    Raises:
+        Exception: If the request returned an error.
+    """
+    credentials = f"{api_key}:{api_secret_key}"
+    b64_credentials = base64.b64encode(credentials.encode("utf-8")).decode("utf-8")
+
+    headers = {
+        "Authorization": f"Basic {b64_credentials}",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    }
+    data = {"grant_type": "client_credentials"}
+
+    response = requests.post(
+        "https://api.twitter.com/oauth2/token", headers=headers, data=data
+    )
+
+    if response.status_code != 200:
+        raise Exception(
+            f"Request returned an error: {response.status_code}, {response.text}"
+        )
+
+    bearer_token = response.json()["access_token"]
+    return bearer_token
 
 
 def get_config():
@@ -8,6 +47,8 @@ def get_config():
     Returns:
         A dictionary containing the configuration values.
     """
+    load_dotenv()
+
     config = {
         "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY"),
         "TWITTER_API_KEY": os.getenv("TWITTER_API_KEY"),
