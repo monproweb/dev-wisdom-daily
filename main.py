1cd5a78b919f079ef194f482d4caf29249764f0f
Thomas Erhel <thomas.erhel@gmail.com>
2023-07-19 07:26:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
 from config import get_config
+import re
 from twitter_manager import TwitterManager
 from threads_manager import ThreadsManager
 from content_generator import ContentGenerator
 
 
 def trigger_tweet(event, context):
-    """
-    Trigger the tweet process to generate a quote, a detailed description, and tweet them as an image.
-
-    Args:
-        event (dict): A dictionary containing data about the triggering event (not used in this function).
-        context (google.cloud.functions.Context): Metadata about the function invocation (not used in this function).
-    """
     config = get_config()
     twitter_manager = TwitterManager(config)
     threads_manager = ThreadsManager(config)
     content_generator = ContentGenerator(config)
 
-    quote, image_url = content_generator.generate_quote_and_image()
+    previous_quotes = twitter_manager.get_previous_quotes()
+    previous_quotes_text = "\n".join(previous_quotes)
 
-    quote_without_hashtags = twitter_manager.tweet_quote_and_image(quote, image_url)
+    quote, quote_text = content_generator.generate_quote(previous_quotes_text)
+    detailed_description = content_generator.generate_detailed_description(quote_text)
+    image_url = content_generator.generate_image(detailed_description)
+
+    twitter_manager.tweet_quote_and_image(quote, image_url)
+    quote_without_hashtags = re.sub(r"#\S+", "", quote)
     threads_manager.thread_quote_and_image(quote_without_hashtags, image_url)
 
 
 def main():
-    """
-    The main function that triggers the tweet process. It sets up the Tweepy API,
-    generates a unique quote and its corresponding image description, and tweets them as an image.
-    """
     config = get_config()
     twitter_manager = TwitterManager(config)
     threads_manager = ThreadsManager(config)
     content_generator = ContentGenerator(config)
 
-    quote, image_url = content_generator.generate_quote_and_image()
+    previous_quotes = twitter_manager.get_previous_quotes()
+    previous_quotes_text = "\n".join(previous_quotes)
 
-    quote_without_hashtags = twitter_manager.tweet_quote_and_image(quote, image_url)
+    quote, quote_text = content_generator.generate_quote(previous_quotes_text)
+    detailed_description = content_generator.generate_detailed_description(quote_text)
+    image_url = content_generator.generate_image(detailed_description)
+
+    twitter_manager.tweet_quote_and_image(quote, image_url)
+    quote_without_hashtags = re.sub(r"#\S+", "", quote)
     threads_manager.thread_quote_and_image(quote_without_hashtags, image_url)
 
 
